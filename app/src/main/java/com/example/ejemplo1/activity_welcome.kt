7e24d6b59e029683f9b3d6a270f765be70b8ba5d
Daniel <danielarratia4@gmail.com>
2024-10-25 22:34:21 -0300

    Prueba de modificacion 4

--- app/src/main/java/com/example/ejemplo1/activity_welcome.kt
+++ app/src/main/java/com/example/ejemplo1/activity_welcome.kt
@@ -2,7 +2,10 @@ package com.example.ejemplo1
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,11 +16,48 @@ class activity_welcome : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        // Configurar el Toolbar
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         // Configurar el botón "Ingresar" para cambiar de pantalla
         val ingresarButton = findViewById<Button>(R.id.button3)
         ingresarButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent) // Navegar a la nueva pantalla
         }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_about -> {
+                Toast.makeText(this, "Se presionó el botón About", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about_1 -> {
+                Toast.makeText(this, "Se presionó el botón About 1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about_2 -> {
+                Toast.makeText(this, "Se presionó el botón About 2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about_3 -> {
+                Toast.makeText(this, "Se presionó el botón About 3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+
+
 }
