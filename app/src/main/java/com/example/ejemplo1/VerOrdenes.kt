47cb97de99eb2902b6dac6803e4b4818bf3216bb
Ivan <ivan.godoy.agg@gmail.com>
2024-11-01 16:55:25 -0300

    Vista Extra Conductor

--- app/src/main/java/com/example/ejemplo1/VerOrdenes.kt
+++ app/src/main/java/com/example/ejemplo1/VerOrdenes.kt
@@ -3,25 +3,24 @@ package com.example.ejemplo1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Conductor : AppCompatActivity() {
+class VerOrdenes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_conductor)
+        setContentView(R.layout.activity_ver_ordenes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val ingresarButton5= findViewById<Button>(R.id.button8)
+        val ingresarButton5= findViewById<Button>(R.id.button12)
         ingresarButton5.setOnClickListener {
-            val intent = Intent(this, VerOrdenes::class.java)
+            val intent = Intent(this, Conductor::class.java)
             startActivity(intent) // Navegar a la nueva pantalla
         }
 
